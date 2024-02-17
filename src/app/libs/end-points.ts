369f633f31596dff8b2ee4e0bf7bc3a926c51505
Guven Eren <guven.eren@bulutmd.com>
2024-02-17 19:32:46 +0300

    installed redux package and created productpage,productlist pages

---
+++ src/app/libs/end-points.ts
@@ -0,0 +1,5 @@
+export const END_POINTS = {
+  PRODUCT: {
+    GET_PRODUCTS: `https://5fc9346b2af77700165ae514.mockapi.io/products`,
+  },
+};
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+import ProductList from "@/src/app/productList/page"
+export default function Home() {
+  return (
+    <ProductList/>
+  );
+}
